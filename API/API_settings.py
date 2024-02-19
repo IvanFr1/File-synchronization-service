d1bf9cc1f877f3f8970041ab21ca14e4ba5e4ca5
Ivan Frolov <ivan.frolov259@gmail.com>
2024-02-19 19:44:41 +0300

    added API

---
+++ API/API_settings.py
@@ -0,0 +1,6 @@
+# from config_data.config import token_api, local_path
+# import requests
+#
+# url = 'https://cloud-api.yandex.net/v1/disk/resources'
+# headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': f'OAuth {token_api}'}
+
